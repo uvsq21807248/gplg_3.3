5a8b81b99f7f0909a2db71eb06e3541223c5e610
uvsq21807248 <atmanihajar09@gmail.com>
2020-03-11 19:56:24 +0100

    création de la class robot type1

---
+++ src/main/java/exercice3/exo/RobotType1.java
@@ -0,0 +1,13 @@
+package exercice3.exo;
+
+public class RobotType1 extends Robot{
+
+	public RobotType1(int position) {
+		super(position);
+		// TODO Auto-generated constructor stub
+	}
+	public void avance() {
+		super.position+=5;
+	}
+
+}
