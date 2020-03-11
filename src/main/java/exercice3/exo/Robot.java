daa26f248667e71700efdf6f32b93c5ce026500c
uvsq21807248 <atmanihajar09@gmail.com>
2020-03-11 19:50:27 +0100

    création de la class robot

---
+++ src/main/java/exercice3/exo/Robot.java
@@ -0,0 +1,11 @@
+package exercice3.exo;
+
+public class Robot {
+	public int position;
+	public Robot(int position) {
+		this.position=position;
+	}
+	public void avance() {
+		this.position+=10;
+	}
+}
